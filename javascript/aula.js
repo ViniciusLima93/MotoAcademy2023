03ce1d5aa64a94449046600b9b0ffd67d33cd387
ViniciusLima93 <vnlima9@gmail.com>
2023-11-08 20:21:18 -0400

    another_commit_0811

--- javascript/aula.js
+++ javascript/aula.js
@@ -41,12 +41,80 @@
 
 
 
-const num = [1,2,3,4]
+// const num = [1,2,3,4]
 
-for(let i = 0; i<num.length; i++) {
-    if(num[i] > 2 ) {
-        (num[i] ** 2)
+// for(let i = 0; i<num.length; i++) {
+//     if(num[i] > 2 ) {
+//         (num[i] ** 2)
+//     }
+//     console.log(num[i]);
+// }
+
+let funcionario = [
+    {
+      nome: 'Euclides',
+      idade: 21,
+      cargo: 'Analista de sistemas',
+      tecnoglogia: 'Node',
+    },
+    {
+      nome: 'Linton',
+      idade: 30,
+      cargo: 'Analista de sistemas',
+      tecnoglogia: 'Tudo',
+    },
+  ];
+
+
+  funcionario.push ({
+      nome: 'Livia',
+      idade: 18,
+      cargo: 'Analista de requesitos',
+      tecnoglogia: 'C#, Java',
+  })
+
+  const IsColab = funcionario.map((li) => {
+    if (li.nome === 'Livia')
+    return {
+        nome : li.nome,
+        idade: li.idade,
+        cargo: li.cargo,
+        tecnologia: li.tecnoglogia
     }
-    console.log(num[i]);
-}
+  })
 
+  console.log(IsColab)
+
+
+  for (const colab of funcionario) {
+    if (colab.nome === 'Euclides')
+    console.log(colab)
+  }
+
+  const isTec = funcionario.filter((li, index)  => {
+    if (index === 0) {
+        li.tecnoglogia = ['C', 'C#','.Net', 'Javascript']
+    }
+    return li
+  })
+
+  console.log(isTec)
+
+
+  console.log(funcionario.map((res) => {
+    res.tecnoglogia = ['Python', 'Ruby','C++']
+    return res
+  }))
+
+  console.log(funcionario.map((res) => {
+    res.tecnoglogia = res.tecnoglogia.reverse();
+    return res;
+  }))
+
+  console.log(funcionario.map((_, index) => {
+     if (typeof _.tecnoglogia === 'string') {
+        _.tecnoglogia = ['abc', 'def', 'xyz']
+     }
+     return _
+    
+  }))
