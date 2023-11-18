a83a6da1ac7e24aa8d45986d5a0225e0cf8610b7
ViniciusLima93 <vnlima9@gmail.com>
2023-11-17 20:16:22 -0400

    class_17112023

--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MyComponentComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- my-first-project/src/app/heroes/heroes.component.ts
+++ my-first-project/src/app/heroes/heroes.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Hero } from '../Hero';
 
 @Component({
   selector: 'app-heroes',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-
+hero: Hero = {
+  id: 0,
+  name: 'Windstorm'
+}
 }
