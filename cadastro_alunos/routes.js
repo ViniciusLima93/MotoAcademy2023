ea116e813ae86fa3009b79c0e8cc990445985228
ViniciusLima93 <vnlima9@gmail.com>
2024-01-15 18:37:53 -0400

    rotas

--- cadastro_alunos/routes.js
+++ cadastro_alunos/routes.js
@@ -15,7 +15,7 @@ router.get('/alunos', (req, res) => {
     })
 });
 
-//endpoitn de Criação de Aluno
+//endpoint de Criação de Aluno
 router.post('/alunos', (req, res) => {
     const {nome, idade} = req.body;
 
@@ -28,6 +28,100 @@ router.post('/alunos', (req, res) => {
             res.status(201).send('Aluno criado com sucesso');
         }
     });
-});  
+}); 
+
+//endpoint de atualização de alunos
+router.put('/alunos/:id', (req, res) => {
+    const {id} = req.params;
+    const {nome, idade} = req.body;
+
+    const query = `UPDATE alunos SET nome='${nome}', idade=${idade} WHERE id_aluno=${id}`;
+
+    db.query(query, (err, result) => {
+        if(err) {
+            res.status(500).send(`Erro ao atualizar aluno: ${JSON.stringify(err)}` );
+
+        }else {
+            res.send('Aluno atualizado com sucesso');
+        }
+    })
+});
+
+//endpoint para exclusão de um aluno
+router.delete('/alunos/:id', (req,res) => {
+    const {id} = req.params;
+
+    const query = `DELETE FROM alunos WHERE id_aluno=${id}`;
+
+    db.query(query, (err, result) => {
+        if(err) {
+            res.status(500).send('Erro ao excluir aluno');
+        }else {
+            res.send('Aluno excluído com sucesso');
+        }
+    });
+})
+
+//Aulas
+
+//endpoint leitura de alunos
+router.get('/aulas', (req, res) => {
+    const query = 'SELECT * FROM aulas';
+
+    db.query(query, (err, result) => {
+        if(err) {
+            res.status(500).send('Erro ao obter aulas');
+        }else {
+            res.json(result)
+        }
+    })
+});
+
+//endpoint de Criação de Aluno
+router.post('/aulas', (req, res) => {
+    const {nome_aula, descricao} = req.body;
+
+    const query = `INSERT INTO aulas(nome_aula, descricao) VALUES('${nome_aula}', '${descricao}')`;
+
+    db.query(query, (err, result) => {
+        if(err) {
+            res.status(500).send('Erro ao criar aulas');
+        }else {
+            res.status(201).send('Aulas criada com sucesso');
+        }
+    });
+}); 
+
+//endpoint de atualização de alunos
+router.put('/aulas/:id', (req, res) => {
+    const {id} = req.params;
+    const {nome_aula, descricao} = req.body;
+
+    const query = `UPDATE aulas SET nome_aula='${nome_aula}', descricao='${descricao}' WHERE id_aula=${id}`;
+
+    db.query(query, (err, result) => {
+        if(err) {
+            res.status(500).send(`Erro ao atualizar aula: ${JSON.stringify(err)}` );
+
+        }else {
+            res.send('Aula atualizada com sucesso');
+        }
+    })
+});
+
+//endpoint para exclusão de um aluno
+router.delete('/aulas/:id', (req,res) => {
+    const {id} = req.params;
+
+    const query = `DELETE FROM aulas WHERE id_aula=${id}`;
+
+    db.query(query, (err, result) => {
+        if(err) {
+            res.status(500).send('Erro ao excluir aula');
+        }else {
+            res.send('Aula excluída com sucesso');
+        }
+    });
+})
 
 module.exports = router;
